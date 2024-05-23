27c12f5e56a2ff0169a728b3453a610fab090389
Vladislav <Laminanonono@mail.ru>
2024-05-24 00:17:46 +0300

    Минимально работоспособная программа для всех файлов (-f)

--- main.go
+++ main.go
@@ -13,14 +13,15 @@ import (
 var NameDir = "testdata"
 
 type file struct {
-	Name  string
-	size  int
-	isDir bool
+	Name    string
+	size    int
+	isDir   bool
+	lastDir bool
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	var check bool
-	filesDir := make([]file, 2)
+	filesDir := make([]file, 0)
+	var dirs []string
 	err := filepath.Walk(NameDir, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			fmt.Fprintf(out, "prevent panic by handling failure accessing a path %q: %v\n", path, err)
@@ -30,58 +31,85 @@ func dirTree(out io.Writer, path string, printFiles bool) error {
 			return filepath.SkipDir
 		}
 		if path != NameDir {
-			filesDir = append(filesDir, file{path, int(info.Size()), info.IsDir()})
+			filesDir = append(filesDir, file{path, int(info.Size()), info.IsDir(), false})
+			if info.IsDir() {
+				dirs = append(dirs, info.Name())
+			}
+
 		}
 		return nil
 
 	})
-	for i, file := range filesDir {
-		filesDir[i].Name, _ = strings.CutPrefix(file.Name, NameDir)
-	}
-	for index, value := range filesDir {
-		var prefStr,sufStr string
-		sufStr = value.Name
-		check = false
-		if value.Name == "" {
+	dirs = append(dirs, strings.Split(filesDir[0].Name, "/")[0])
+
+	var maxLevel int
+	for i := len(filesDir) - 1; i > 0; i-- {
+		if filesDir[i].Name == "" {
 			continue
 		}
-		count := strings.Count(value.Name, "\\")
-		for count > 0 {
-			if count > 1 {
-				str, _, _ := strings.Cut(sufStr, "\\")
-				prefStr += str + "\\"
-				_, sufStr, _ = strings.Cut(sufStr, "\\")
-				fmt.Println("prefStr: ", prefStr, ";sufStr: ", sufStr, "; Count:", count)
-				 Если в будущем есть prefStr тогда "|"
-			}
-			if count == 1 {
-				for i := index + 1; i < len(filesDir); i++ {
-					if value.Name[:strings.LastIndex(value.Name, "\\")] == filesDir[i].Name[:strings.LastIndex(filesDir[i].Name, "\\")] {
-						check = true
-						fmt.Fprint(out, "├───")
-						break
-					}
-				}
-				if !check {
-					fmt.Fprint(out, "└───")
-				}
-			}
-			count--
+		parent := strings.Split(filesDir[i].Name, "/")[len(strings.Split(filesDir[i].Name, "/"))-2]
+		if len(strings.Split(filesDir[i].Name, "/")) > maxLevel {
+			maxLevel = strings.Count(filesDir[i].Name, "/")
 		}
-		fmt.Fprint(out, value.Name[strings.LastIndex(value.Name, "\\")+1:])
-		if !value.isDir {
-			if value.size > 0 {
-				fmt.Fprint(out, " ("+strconv.Itoa(value.size)+"b)\n")
-			} else {
-				fmt.Fprint(out, " (empty)\n")
-			}
-		} else {
-			fmt.Fprintln(out)
+		index, contain := find(dirs, parent)
+		if contain {
+			dirs[index] = ""
+			filesDir[i].lastDir = true
 		}
-		// fmt.Println(Name)
+
 	}
-	fmt.Println(filesDir)
+
+	levelPrint := make([]bool, maxLevel)
+	for i := range levelPrint {
+		levelPrint[i] = true
+	}
+	var str string
+	for i, value := range filesDir {
+		dir := strings.Split(value.Name, "/")
+		if i > 0 && len(dir) < len(strings.Split(filesDir[i-1].Name, "/")) {
+			for i := range levelPrint {
+				levelPrint[i] = true
+			}
+		}
+		levelIndex := 0
+		for i := len(dir); i > 2; i-- {
+			if levelPrint[levelIndex] {
+				str += "│"
+			}
+			levelIndex++
+			str += "\t"
+		}
+		if value.lastDir {
+			str += "└───"
+		} else {
+			str += "├───"
+		}
+		str += dir[len(dir)-1]
+		if !value.isDir {
+			size := strconv.Itoa(value.size)
+			if size == "0" {
+				size = "empty"
+			} else {
+				size += "b"
+			}
+			str += " (" + size + ")"
+		}
+		str += "\n"
+		if value.lastDir {
+			levelPrint[strings.Count(value.Name, "/")-1] = false
+		}
+	}
+	out.Write([]byte(str))
 	return err
+}
+
+func find(arr []string, val string) (int, bool) {
+	for i, value := range arr {
+		if val == value {
+			return i, true
+		}
+	}
+	return -1, false
 }
 
 func main() {
